896b7b85beb58828027cdb9e57d67a74938e6366
sora0116 <132332957+sora0116@users.noreply.github.com>
2025-05-11 17:05:18 +0900

    Fix: remove outDir

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sora0116.github.io/',
-	outDir: "./docs"
+	site: 'https://sora0116.github.io/'
 });
